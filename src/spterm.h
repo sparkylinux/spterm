7afaaa57da03b6e2ab58ef2abbcd84ebbf04ae8e
pavroo <pavroo@onet.eu>
2021-01-12 17:30:01 +0100

    0.3.2

--- src/spterm.h
+++ src/spterm.h
@@ -1,7 +1,6 @@
 /*
- * spterm forked from k3rmit by pavroo <pavroo@onet.eu> May 16, 2020
- * k3rmit, a VTE-based terminal emulator that aims to be simple, fast and effective.
- * Copyright (C) 2019 by orhun <https://www.github.com/orhun>
+ * kermit, a VTE-based, simple and froggy terminal emulator.
+ * Copyright (C) 2019-2020 by orhun <https://www.github.com/orhun>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,14 +17,14 @@
  */
 
 #include <vte/vte.h>
-#define TERM_NAME "Sparky Terminal"
-#define TERM_VERSION "0.1.2"
+#define TERM_NAME "SparkyTerminal"
+#define TERM_VERSION "0.3.2"
 #define TERM_FONT "Monospace"
 #define TERM_FONT_DEFAULT_SIZE 10
 #define TERM_LOCALE "en_US.UTF-8"
 #define TERM_OPACITY 0.96
 #define TERM_WORD_CHARS "-./?%&#_=+@~"
-#define TERM_BACKGROUND 0x0E0D0D
+#define TERM_BACKGROUND 0x000000
 #define TERM_FOREGROUND 0xffffff
 #define TERM_BOLD_COLOR 0xffffff
 #define TERM_CURSOR_COLOR 0xffffff
@@ -38,16 +37,16 @@
 #define TERM_ATTR_COLOR "\x1b[34m"
 #define TERM_ATTR_DEFAULT "\x1b[39m"
 
-static GtkWidget* getTerm();
+static GtkWidget *getTerm();
 static void parseSettings();
-static int configureTerm(GtkWidget* term);
+static int configureTerm(GtkWidget *term);
 static int setTermFont(GtkWidget *term, int fontSize);
 static gboolean termOnChildExit(VteTerminal *term,
-    gint status, gpointer userData);
+                                gint status, gpointer userData);
 static gboolean termOnKeyPress(GtkWidget *widget,
-    GdkEventKey *key, gpointer gptr);
+                               GdkEventKey *key, gpointer gptr);
 static gboolean termOnTitleChanged(GtkWidget *term,
-    gpointer gptr);
+                                   gpointer gptr);
 static gboolean termOnResize(GtkWidget *widget,
-    GtkAllocation *allocation,
-    gpointer userData);
+                             GtkAllocation *allocation,
+                             gpointer userData);
